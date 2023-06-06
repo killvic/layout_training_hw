6aef6bb79558d43c73903be35ef35e8434a355e2
killvic <killvic2000@gmail.com>
2023-06-06 18:04:05 +0300

    URLs to Images moved inside const vals for easier future edit
    TO-DO: Put image downloading inside a separate thread

--- app/src/main/java/com/example/layouttraining/MainActivity.kt
+++ app/src/main/java/com/example/layouttraining/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.layouttraining
 
 
-import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.HorizontalScrollView
+import android.util.Log
 import android.widget.ImageView
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -14,6 +12,14 @@ import com.example.layouttraining.Data.ReviewClass
 import com.example.layouttraining.Data.TagClass
 import com.example.layouttraining.Data.UserClass
 import com.example.layouttraining.GameTagsRecycler.AdapterForGameScreen
+
+// TODO: FIX IMAGE LOADING
+// TODO: IMPLEMENT COROUTINE FOR IMAGE LOADING
+
+private const val BACKGROUND_IMAGE_URL = "https://s3-alpha-sig.figma.com/img/284d/401f/f98fb91fbba05e15f99cdcd8e4f9d673?Expires=1687132800&Signature=EXkWaKQQpK~~Fm9tWRf3bgw-s0LP5inq-BKiTnyy6cvuCfXnoqV78xVxziyeyD5mTAGgyPdd1xDHKzqXtRewBnGKjp~PWEh7JARYUI7aqFHtuJw5JxzpqaP9DUlToGfgiBDv1r4Y0Wbee2VgHPhP7iMIDarUVMjVs-xFLh3F4ZpCqcJ~jQOwBDVw9ypGaG4tjMfhf9tK3c69r1ofH-cevlkf4xzyx0I3WusSrRlf6bTku7MiI53xqjy2Ng4zm1h9mcJQMP9ufKumI0wFqJmIZEQFnVkAXio6kLYRfripMuwS-l-4HJHh3SVFsKL8t1oOnxqEBRIm7r3GnxwQVIcGPQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+private const val GAME_ICON_URL = "https://s3-alpha-sig.figma.com/img/cf52/a010/d2d8badd19c3de46d88b8a74f80aaef6?Expires=1687132800&Signature=BIcrDlpvWzdhkPHseksvkamUOp3wGcfhw5-wn81p7V6HKcbHf9UhCjr6pmv4Gu-IdwQ-ZdxSNPO2g9GBAU2Jrb76XKlJZgNVp1YRwGoqjPjtip7ooCgJJV73B222nFbHZSOu9Je8Jnsc~Cex3-YJ9YljizKVN8fieroK9aC3EITt1CsQRCcnkBXctyFN2HSuwCm0QmQBOGOwiRuN0mOTcUa5cxFoXBh7eZiOfUYZR8raEXEoJ~UaVj5Bz03ctoMxVaEbSCB1V33QpCfMPaNxRT9H1uHO0ITbdlg6HnrWi50C38HgDhgitaBt228mfoayv1Y3CxEJC63a10FE4WP2Og__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+private const val AVATAR_1_URL = "https://s3-alpha-sig.figma.com/img/50d7/87dc/de907b9c097cd7f9ce6f8703ed2b03f1?Expires=1687132800&Signature=YcnCq5UpYnWzR2JuZL9QezTkuc200UCzSZ5j4xVmU~8NMlQ7X-t9CKPELQi7-aj0C70yzDrXigXrDFXUqpGNq~nJy4jpvBZ3M3SOl9hbinizfcN5YRnOprQz3K01Vm4TJk1PvyBtlO8myJdkkHqQ-7KD4y9lMaEq6uzR0F69Gv2Sbt70QzUf3D~WhRRkpZxoeTVNtB-Kx2Z-b6s8D9DxnTljUOmuWcFWMtGZZsmW6pV4EVVoYSafTHYz~ZI3g43VlXHppnyWnTysDSwhehSi8Vv5yVJYnIFBGZs8bw8RhAzTdgsmwNCCdtqIp~rJyl6mUuIvvMfJiAbLrXIQqQAW9g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+private const val AVATAR_2_URL = "https://s3-alpha-sig.figma.com/img/6212/e251/4d28d9fa8e1c1ffdeb79b8525b209e00?Expires=1687132800&Signature=ZhVo0kWgAJUS9~NlCIT69MxZN7GpFigJ4ro2Mv5zTo8IgV7H8Z6DUKg~-952iJs-cgNMuJqm0nGV~1ihoQ0uzgSkrncq59uqeMgrKnrN4kIdQjPWeB1u11shclNp6DyKTKHfZ7otEs6enmSd-JpSirvuJlf6Y6BY3krbJnQOAs~maiLeOFKM3NYLLWKwOtCmO1hos9qrpg--LQr11UcKOydzKkxi4gv3L7MqozfuGcIaHhkYEM4T0urCA8zh9fWOs88ZcEKuHrPg2usuy3PdefiQUTpIIO5Zyhe-UbQoc1UQ67CLRq349ewO7MXno-2pd~DpvNmd3ew8FAsuyYyYQg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var ivBackgroundImage: ImageView
@@ -32,8 +38,16 @@ class MainActivity : AppCompatActivity() {
         )
 
         var reviewList = listOf(
-            ReviewClass(1, 1550102400000, "Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers.", UserClass(1, "https://s3-alpha-sig.figma.com/img/50d7/87dc/de907b9c097cd7f9ce6f8703ed2b03f1?Expires=1680480000&Signature=qkYk~rqJevw-IlMT4BcLO70mAGCagK-FGaSzyE38nziDm-YTjgDJHNoAffk6SDLNQQ-db8WXbQachiR7BuXf9akGyfoHCJVphwCMPHyqYuV1-V-z8M7r6uSaSwMZc5C1JhodpENx02eBSZOeTibkGHLeFPCvShXfoDo~2CA7rEEpbq4D7rSLMgJpEGjNU7OGEOg0VvtgwUBvvk7zq-zlprackUsgGW2M6guUUqUjEfrmn1s0wTdcv9T0F2wuaRwTEmMGpSQSlUvcf3zYShp78JmT9tCvfSpS7P0xVfvPAHvbSl2roWMQ6I0cXsAJo8FklvDV-47~hja0vp8-XT8QXA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4", "Auguste Conte")),
-            ReviewClass(2, 1550102400000, "“Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers.”", UserClass(1, "https://s3-alpha-sig.figma.com/img/6212/e251/4d28d9fa8e1c1ffdeb79b8525b209e00?Expires=1680480000&Signature=qrUcXb81tJQUQ2EYqd9bqBrqaHUxyrkf8lFI75V9uj7D3ADUJtiHSMWf5W4To2Ldrct4mrAoHSTHsqpzEJVePaj~0Dt5pGhMeO9tJwi2v8MkiNcTENIRze-5yUJp-kICE2A65q12oIg2fcg2l6H4JauZ7crdMeQdCrJJ2TvrTJv3SYPAu00IrRy2iuE3JINiaAuh2NZFw58tVSvt7pmWdb3W4UV365VsWpv0JS51eY~rXn4yTeUGkYr72Li38z1rNBPyTFgVv04GNki68GI7ypWfBVwQTYGZEhSrkYgYGo7tjCkUUmiMbqdgILcrZ-qSzdiGTjlteGD36~5UamwAKQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4", "Jang Marcelino"))
+            ReviewClass(
+                1,
+                1550102400000,
+                "Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers.",
+                UserClass(1, AVATAR_1_URL, "Auguste Conte")),
+            ReviewClass(
+                2,
+                1550102400000,
+                "“Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers.”",
+                UserClass(1, AVATAR_2_URL, "Jang Marcelino"))
         )
 
         gameImagesInitialization()
@@ -49,16 +63,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun gameImagesInitialization() {
         ivBackgroundImage = findViewById(R.id.ivBackgroundImage)
-        val backgroundImageURL = "https://s3-alpha-sig.figma.com/img/284d/401f/f98fb91fbba05e15f99cdcd8e4f9d673?Expires=1680480000&Signature=SOTU7egX1t~82UR~RcV3qhyYiC-dhLa~~boKqlHs6RF58sMEMROyeaie~68HxX1X1rcdI5P7awo0twv05gBX3IYDHg-3ba~-ap2PO3-5moC0am9Q7tHv1DTL94Z8pxz3ysgn8BhgBZnpYHyxvqV2Cbo8QqoKDIpESCKo8JGqU3NSUVXRGRZw2TPunD6gDZIbQ9qOgqPvpK4HxGKmSGppxg-HHD5eFNg47b5GWVjNU6eWB9VcewqjxG-L0q-UAShNJYLQhO3rj7O6t2IFd5W0oNWaPZ2EcJW6mQ-w2WM3lsvsTsMR1lTidtDliJD6skZJFLUeZB7hDltMpnnvpDfLag__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        Glide.with(this)
-            .load(backgroundImageURL)
+            Glide.with(this)
+            .load(BACKGROUND_IMAGE_URL)
             .centerCrop()
             .into(ivBackgroundImage)
-
         ivGameIcon = findViewById(R.id.ivGameIcon)
-        val gameIconImageURL = "https://s3-alpha-sig.figma.com/img/cf52/a010/d2d8badd19c3de46d88b8a74f80aaef6?Expires=1680480000&Signature=nwF6kDI34GCW1oOURfjUMol6UlM3K5468t7K01mqEyBMVzA1n48oLi~8dYpblQD0RScr3zkJEMREOugl1tMTUJh8-W2MsFZG-akr-H2MuE3MoWSXrImkpx3tzN576tuhog0XIy8d9DJUOZGFQPjQQUTq9MNAsJ6IZQmerJ0Nbq-eUZspGKt5XTcH5yC~3UAWP4yvCy3lOeeNHD~HpDxhZYgclzRBtCq1~2VImE2~M1i92-xaNHW1RHQFFY1XQsGcWb9O8LJ7dW1ByXdJEyMeuDh38T2r0EQrxMpBqjd3RzpS5vYe0bdK1mbszzsvPfeibwgjzMWO1OOUF4sNBafeuQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        Glide.with(this)
-            .load(gameIconImageURL)
+            Glide.with(this)
+            .load(GAME_ICON_URL)
             .centerCrop()
             .into(ivGameIcon)
     }
